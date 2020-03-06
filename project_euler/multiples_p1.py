e4b43c8f83da2523380fc8bd52bbe58ed86274b0
nachiketbhujbal <nachiketbhujbal@outlook.com>
2020-03-06 02:30:16 -0500

    tiny formatting changes

--- project_euler/multiples_p1.py
+++ project_euler/multiples_p1.py
@@ -1,14 +1,13 @@
 #!/usr/local/bin/python3
 # date: 2020-03-05
 # author: Nachiket Bhujbal
-'''
-Problem #1:
-
+# Problem #1:
+    '''
     If we list all the natural numbers below 10 that are multiples
     of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
     Find the sum of all the multiples of 3 or 5 below 1,000.
-'''
+    '''
 # note: gentools in https://github.com/nachiketbhujbal/playground/py
 import sys
 import time

34ff0c12367494a32ad17f803d1b28b1559fc97c
nachiketbhujbal <nachiketbhujbal@outlook.com>
2020-05-15 01:59:03 -0400

    most general & fast solution I could come up with -- now i'm sleepy

--- project_euler/largestprimefactor_p3.py
+++ project_euler/largestprimefactor_p3.py
@@ -8,20 +8,49 @@ What is the largest prime factor of the number 600851475143?
 '''
 # note: gentools in https://github.com/nachiketbhujbal/playground/py
 import sys
+import math
 import time
 from gentools import custominput, customoutput
 
+# This solution is not efficient on numbers greater than 20 digits,
+# but does work, given enough time / resources
+# many assumptions were made to speed up performance for up to 20 digits
 def prime_factors(n):
+    if n < 2:
+        print('BAD INPUT: Must enter an integer greater than 1. EXITING')
+        sys.exit()
+
     factors = []
     while n != 1:
-        for i in range(2, n + 1):
-            if n % i == 0: # this number divides w/o remainder
+        print(n)
+        start = factors[-1] if len(factors) > 0 else 2
+        endat = math.ceil(math.sqrt(n)) + 1 # only need to check up to sqrt(n)
+        print('start: {}, end: {}'.format(start, endat))
+        for i in range(start, endat):
+            # print('dividing {} / {}'.format(n, i))
+            if n % i == 0: # if n divides w/o remainder
                 factors.append(i)
-                break
+                break # exit this for loop after we find another prime factor
 
-        n = n // factors[-1]
-        if n == 0:
-            sys.exit(1)
+        # The following three conditions test for end cases to return our list
+        # print(factors)
+        if len(factors) < 1: # this means this is already prime:
+            return [n]
+
+        if n > factors[-1]: # n should be > than our last factor or we are done
+            # n / last factor has remainder -> it is prime, so we are also done
+            if n % factors[-1] != 0:
+                factors.append(n)
+                return factors
+
+            # print('Dividing n = {} by last factor: {}'.format(n, factors[-1]))
+            n = n // factors[-1]
+        else:
+            return factors
+
+        if n == 0: # in case of erroneous divide, just exit and warn
+            print('ERROR: Erroneous n = 0 condition met. EXITING')
+            sys.exit()
 
     return factors
 
